c73d0c5bad8e3685dae4539b94058557bd47ecb0
Harish <harish.upad@gmail.com>
2014-05-07 17:26:05 -0400

    Initial Commit

---
+++ MyDiary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyDiary
+//
+//  Created by Harish Upadhyayula on 5/7/14.
+//  Copyright (c) 2014 tensor apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TAAppDelegate class]));
+    }
+}
